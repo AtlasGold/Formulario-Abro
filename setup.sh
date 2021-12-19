5d1c74b5ea6ca8f6ddb1e5e57cac4e18a399b161
Luan Vinicius <luan.vini31@hotmail.com>
2021-12-18 23:34:44 -0300

    manter as cores

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 echo "[theme]
-primaryColor=`#020202`
-backgroundColor=`#c4c3c3`
-secondaryBackgroundColor=`#ebd316`
-font = `sans serif`
+primaryColor='#020202'
+backgroundColor='#c4c3c3'
+secondaryBackgroundColor='#ebd316'
+font = 'sans serif'
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
