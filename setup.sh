deac998fd6981501deed37a387a4df4ac42e5866
Luan Vinicius <luan.vini31@hotmail.com>
2021-12-18 23:29:38 -0300

    manter as cores

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+mkdir -p ~/.streamlit/
+echo "[theme]
+primaryColor=`#020202`
+backgroundColor=`#c4c3c3`
+secondaryBackgroundColor=`#ebd316`
+font = `sans serif`
+[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
+
