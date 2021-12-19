e63037b936c838a15f42960f71c125a1499f87cc
Luan Vinicius <luan.vini31@hotmail.com>
2021-12-19 18:10:36 -0300

    teste

--- conexão/conexao.py
+++
@@ -1,5 +0,0 @@
-#conexão
-
-import mysql.connector
-cnxn = mysql.connector.connect(host="sql10.freemysqlhosting.net", user="sql10459891", passwd="ZEVEuMyb52", db="sql10459891")
-cursor = cnxn.cursor()
